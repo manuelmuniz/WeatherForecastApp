f48120436ea9b96e11e6503f41d38523f98e9953
Manuel Muñiz <mamm185x@gmail.com>
2021-10-09 09:21:02 -0500

    Agregar archivos de proyecto.

---
+++ WeatherForecastApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherForecastApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
